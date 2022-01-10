94a42b7983050ab9048dcb8a5cb00d169cb26e1a
emage <emage@a.b>
2022-01-10 14:24:08 +0400

    MyModal

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import {useMemo, useState} from "react";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
 import SearchSortForm from "./components/SearchSortForm";
+import MyButton from "./components/UI/button/MyButton";
+import MyModal from "./components/UI/modal/MyModal";
 
 function App() {
     const [posts, setPosts] = useState([{id: 1, title: 'First post', body: 'Body of post'}]);
     const [filter, setFilter] = useState({sortBy: '', query: ''});
+    const [modal, setModal] = useState(false);
     const sortedPosts = useMemo(
         () => filter.sortBy
             ? [...posts].sort((a, b) => a[filter.sortBy].localeCompare(b[filter.sortBy]))
@@ -19,6 +22,7 @@ function App() {
     );
     const addPost = (newPost) => {
         setPosts([newPost, ...posts])
+        setModal(false);
     };
     const deletePost = (delPost) => {
         setPosts(posts.filter(post => post.id !== delPost.id))
@@ -27,7 +31,10 @@ function App() {
     return (
         <div className="App">
             <h1>Новости</h1>
-            <PostForm addPost={addPost}/>
+            <MyButton onClick={() => setModal(true)}>Добавить новость</MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm addPost={addPost}/>
+            </MyModal>
             <SearchSortForm filter={filter} setFilter={setFilter}/>
             <PostList posts={sortedAndFilteredPosts} deletePost={deletePost}/>
         </div>
