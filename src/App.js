41de42d8dde567f84676430592cd05d509f8c5fc
emage <emage@a.b>
2022-01-12 00:22:35 +0400

    user hook - usePosts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import './App.css';
-import {useMemo, useState} from "react";
+import {useState} from "react";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
 import SearchSortForm from "./components/SearchSortForm";
 import MyButton from "./components/UI/button/MyButton";
 import MyModal from "./components/UI/modal/MyModal";
+import {usePosts} from "./hooks/usePosts";
 
 function App() {
     const [posts, setPosts] = useState([{id: 1, title: 'First post', body: 'Body of post'}]);
     const [filter, setFilter] = useState({sortBy: '', query: ''});
     const [modal, setModal] = useState(false);
-    const sortedPosts = useMemo(
+    const sortedAndFilteredPosts = usePosts(posts, filter.sortBy, filter.query);
+
+    /*const sortedPosts = useMemo(
         () => filter.sortBy
             ? [...posts].sort((a, b) => a[filter.sortBy].localeCompare(b[filter.sortBy]))
             : [...posts],
-        [filter, posts]
+        [filter.sortBy, posts]
     );
     const sortedAndFilteredPosts = useMemo(
         () => sortedPosts.filter(post => post.body.toLowerCase().includes(filter.query.toLowerCase())),
-        [sortedPosts]
-    );
+        [filter.query, sortedPosts]
+    );*/
     const addPost = (newPost) => {
         setPosts([newPost, ...posts])
         setModal(false);
