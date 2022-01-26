f6860fea32c8f16eb709b6ed79d0c55f69b5d911
emage <emage@a.b>
2022-01-26 22:39:52 +0400

    Articles CSS

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -4,16 +4,14 @@ import axios from "axios";
 import './Articles.css';
 
 const Articles = () => {
+    // React Query
     const queryClient = useQueryClient();
     const query = useQuery('articles', getArticles);
-    const mutation = useMutation(postArticle, {
+    const mutationCreate = useMutation(postArticle, {
         onSuccess: () => {
             queryClient.invalidateQueries('articles');
         },
     });
-    const [files, setFiles] = useState();
-    const [title, setTitle] = useState('');
-    const [body, setBody] = useState('');
 
     async function getArticles() {
         const response = await axios.get('http://localhost:1337/api/articles?populate=cover');
@@ -25,13 +23,17 @@ const Articles = () => {
         return axios.post('http://localhost:1337/api/articles', newArticle);
     }
 
+    const [files, setFiles] = useState();
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+
     async function handleSubmit(e) {
         e.preventDefault();
         const formData = new FormData();
         formData.append('files.cover', files[0]);
         const myData = { title, body };
         formData.append('data', JSON.stringify(myData));
-        mutation.mutate(formData);
+        mutationCreate.mutate(formData);
     }
 
     if (query.isLoading) {
@@ -44,40 +46,47 @@ const Articles = () => {
 
     return (
         <div className="articles">
-            <h2>Articles from strapi-backend</h2>
-            <ul>
-                {query.data.data.map(article =>
-                    <li key={article.id} className="article__item">
-                        <p>id: {article.id}</p>
-                        <p>Заголовок: {article.attributes.title}</p>
-                        <p>Описание: {article.attributes.body}</p>
-                        <p>
-                            {article.attributes?.cover?.data?.attributes?.url
-                                ? <img width="20%" src={`http://localhost:1337${article.attributes?.cover?.data?.attributes?.url}`}/>
-                                : null
-                            }
-                        </p>
+            <div>
+                <h2>Articles from strapi-backend</h2>
+                <ul className="article__list">
+                    {query.data.data.map(article =>
+                        <li key={article.id} className="article__item">
+                            <p>id: {article.id}</p>
+                            <p>Заголовок: {article.attributes.title}</p>
+                            <p>Описание: {article.attributes.body}</p>
+                            <p>
+                                {article.attributes?.cover?.data?.attributes?.url
+                                    ? <img width="20%" src={`http://localhost:1337${article.attributes?.cover?.data?.attributes?.url}`}/>
+                                    : null
+                                }
+                            </p>
 
-                    </li>
-                )}
-            </ul>
-            <h3>Добавление статей</h3>
-            <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Заголовок..." value={title} onChange={e => setTitle(e.target.value)}/>
-                <input type="text" placeholder="Описание..." value={body} onChange={e => setBody(e.target.value)}/>
-                <input type="file" name="cover" onChange={e => setFiles(e.target.files)}/>
-                <button type="submit">Добавить статью с файлом</button>
-            </form>
-            <button onClick={() => {
-                mutation.mutate({
-                    data:
-                        {
-                            title: 'New Article title',
-                            body: 'Here some body of the new article'
-                        }
-                })
-            }}>Добавть стандартную статью
-            </button>
+                        </li>
+                    )}
+                </ul>
+            </div>
+            <div>
+                <h3>Добавление статей c файлом</h3>
+                <form className="article__form" onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Заголовок..." value={title} onChange={e => setTitle(e.target.value)}/>
+                    <input type="text" placeholder="Описание..." value={body} onChange={e => setBody(e.target.value)}/>
+                    <input type="file" name="cover" onChange={e => setFiles(e.target.files)}/>
+                    <button type="submit">Добавить статью с файлом</button>
+                </form>
+            </div>
+            <div>
+                <h3>Добавление стандартной статьи</h3>
+                <button onClick={() => {
+                    mutationCreate.mutate({
+                        data:
+                            {
+                                title: 'New Article title',
+                                body: 'Here some body of the new article'
+                            }
+                    })
+                }}>Добавть стандартную статью
+                </button>
+            </div>
         </div>
     );
 };
